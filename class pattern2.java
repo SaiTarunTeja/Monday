20ced7948b705b6827b4eaa0f87233cb014076f8
github_SaiTarunTeja <github_saitarunteja1729@gmail.com>
2023-08-21 12:02:54 +0530

    first commit

---
+++ class pattern2.java
@@ -0,0 +1,14 @@
+class pattern2
+{
+  public static void main(String[] args)
+  {
+    int x,y;
+    for(x=0;x<=4;x++)
+    {
+      for(y=0;y<=x;y++)
+      {
+         System.out.print( y +"  ");
+      }
+      System.out.println();
+    }
+  }
